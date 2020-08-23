615d411c8d644109da3080362e737dfa4b4104d3
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-08-24 01:11:46 +0900

    css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,54 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<div class="demo-description">
+        <h1 class="demo-description__title">Full Course Dinner</h1>
+        <p class="demo-description__p">Resize the browser to see the <picture> effect.</p>
+    </div>
 
-        <title>Laravel</title>
+    <div class="demo coursemeal">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+        <!-- APPETIZERS-->
+        <div class="coursemeal__div ">
+            
+            <picture class="responsive-img">
+                <source media="(min-width: 992px)" srcset="https://i.postimg.cc/jSTZmp8H/appetizer-img-1.jpg">
+                <source media="(min-width: 767px)" srcset="https://i.postimg.cc/1RTdCYhB/appetizer-img-2.jpg">
+                <source srcset="https://i.postimg.cc/TwnctQr7/appetizer-img-3.jpg">
+                <img src="https://i.postimg.cc/jSTZmp8H/appetizer-img-1.jpg" alt="Appetizer" >
+            </picture>     
+            
+            <div class="coursemeal-info">
+                <a href="#" class="coursemeal-info__link">Appetizer</a>
             </div>
         </div>
-    </body>
-</html>
+
+        <!-- MAIN COURSE-->
+        <div class="coursemeal__div">
+            
+            <picture class="responsive-img">
+                <source media="(min-width: 992px)" srcset="https://i.postimg.cc/V6NCCRK0/maindish-img-1.jpg">
+                <source media="(min-width: 767px)" srcset="https://i.postimg.cc/9XYj186s/maindish-img-2.jpg">
+                <source srcset="https://i.postimg.cc/pLY8dt4q/maindish-img-3.jpg">
+                <img src="https://i.postimg.cc/V6NCCRK0/maindish-img-1.jpg" alt="Main Course" >
+            </picture> 
+
+            <div class="coursemeal-info">
+                <a href="#" class="coursemeal-info__link">Main Course</a>
+            </div>            
+        </div>
+
+        <!-- DESSERTS-->
+        <div class="coursemeal__div">
+            
+            <picture class="responsive-img">
+                <source media="(min-width: 992px)" srcset="https://i.postimg.cc/9FfLh6ZZ/dessert-img-1.jpg">
+                <source media="(min-width: 767px)" srcset="https://i.postimg.cc/FsRXBnKn/dessert-img-2.jpg">
+                <source srcset="https://i.postimg.cc/ZKcFCVFZ/dessert-img-3.jpg">
+                <img src="https://i.postimg.cc/ZKcFCVFZ/dessert-img-1.jpg" alt="Desserts">
+            </picture>
+
+            <div class="coursemeal-info">
+                <a href="#" class="coursemeal-info__link">Dessert</a>
+            </div>              
+        </div>
+
+
+    </div>
